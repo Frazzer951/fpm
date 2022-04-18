88b9c3793f1723a04c6ca49ae65a8a5fea05f6d6
Luke Eltiste <luke343279@gmail.com>
2022-04-18 12:51:10 -0700

    parse template

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, Subcommand};
+use serde::Deserialize;
 use serde_json;
 
 #[derive(Parser)]
@@ -25,11 +26,36 @@ enum Commands {
     },
 }
 
-fn load_template(language: &String) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
+fn load_template(language: &String) -> Result<Template, Box<dyn std::error::Error>> {
     let filename = format!("templates/{language}.json");
     let f = std::fs::File::open(filename)?;
-    let d: serde_json::Value = serde_json::from_reader(f)?;
+    let d: Template = serde_json::from_reader(f)?;
     Ok(d)
+}
+
+#[derive(Debug, Deserialize)]
+struct Template {
+    folders: Option<Vec<Folder>>,
+    files: Option<Vec<File>>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Folder {
+    name: String,
+    files: Vec<File>,
+    sub_folders: Option<Vec<Folder>>,
+}
+
+#[derive(Debug, Deserialize)]
+struct File {
+    name: String,
+    lines: Vec<String>,
+}
+
+impl Folder {
+    pub fn add_sub_folder(&mut self, folder: Folder) {
+        self.sub_folders.get_or_insert_with(Vec::new).push(folder);
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -43,10 +69,32 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             ..
         } => {
             println!("Creating project named {}", name);
+
+            let mut proj_folder = Folder {
+                name: name.clone(),
+                files: vec![File {
+                    name: "README.md".to_string(),
+                    lines: vec![format!("# {name}")],
+                }],
+                sub_folders: None,
+            };
+
             if *template {
-                let yaml_template = load_template(language)?;
-                println!("{:?}", yaml_template["c++"]);
+                let yaml_template: Template = load_template(language)?;
+                //println!("{:#?}", yaml_template);
+
+                for file in yaml_template.files.unwrap() {
+                    //println!("file: {:#?}", file);
+                    proj_folder.files.push(file);
+                }
+
+                for folder in yaml_template.folders.unwrap() {
+                    //println!("folder: {:#?}", folder);
+                    proj_folder.add_sub_folder(folder);
+                }
             }
+
+            //println!("{:#?}", proj_folder);
         }
     }
 
