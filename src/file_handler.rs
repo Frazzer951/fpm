79bebb05e5fba7f2ee99871d97282d128c401c23
Luke Eltiste <luke343279@gmail.com>
2022-06-07 17:58:47 -0700

    Refactoring

---
+++ src/file_handler.rs
@@ -0,0 +1,114 @@
+use std::{fmt, fs};
+
+use clap::{ArgEnum};
+use serde::{Deserialize, Serialize};
+
+use crate::{CONFIG_FILENAME, PROJECT_DB_FILENAME, PROJECT_NAME};
+
+// region -- Custom Errors
+type Result<T> = std::result::Result<T, FileError>;
+
+#[derive(Debug, Clone)]
+pub enum FileError {
+    LoadingError,
+    ParsingError,
+}
+// endregion
+
+impl fmt::Display for FileError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            FileError::LoadingError => {
+                write!(f, "Failed to load config file")
+            },
+            FileError::ParsingError => {
+                write!(f, "Failed to parse config file")
+            },
+        }
+    }
+}
+
+// region -- Config Struct
+#[derive(Deserialize, Serialize, Default, Debug)]
+pub struct Config {
+    #[serde(default)]
+    pub base_dir: Option<String>,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
+pub enum ConfigOptions {
+    BaseDir,
+}
+// endregion
+
+// region -- Project Struct
+#[derive(Deserialize, Serialize, Default, Debug)]
+pub struct Project {
+    pub name:      String,
+    pub directory: String,
+    #[serde(default)]
+    pub category:  Option<String>,
+    #[serde(default)]
+    pub p_type:    Option<String>,
+}
+// endregion
+
+pub fn load_config() -> Result<Config> {
+    let mut config_dir = dirs::config_dir().unwrap();
+    config_dir.push(PROJECT_NAME);
+    config_dir.push(CONFIG_FILENAME);
+
+    let contents = match fs::read_to_string(config_dir) {
+        Ok(c) => Ok(c),
+        Err(_) => Err(FileError::LoadingError),
+    }?;
+
+    match toml::from_str(&contents) {
+        Ok(d) => Ok(d),
+        Err(_) => Err(FileError::ParsingError),
+    }
+}
+
+pub fn load_projects() -> Result<Vec<Project>> {
+    let mut projects_dir = dirs::config_dir().unwrap();
+    projects_dir.push(PROJECT_NAME);
+    projects_dir.push(PROJECT_DB_FILENAME);
+
+    let contents = match fs::read_to_string(projects_dir) {
+        Ok(c) => Ok(c),
+        Err(_) => Err(FileError::LoadingError),
+    }?;
+
+    match serde_json::from_str(&contents) {
+        Ok(d) => Ok(d),
+        Err(_) => Err(FileError::ParsingError),
+    }
+}
+
+pub fn save_config(config: Config) {
+    let mut config_dir = dirs::config_dir().unwrap();
+    config_dir.push(PROJECT_NAME);
+
+    // make sure path exists
+    fs::create_dir_all(config_dir.clone()).unwrap();
+
+    config_dir.push(CONFIG_FILENAME);
+
+    // save config to config_dir
+    let contents = toml::to_string(&config).unwrap();
+    fs::write(config_dir, contents).unwrap();
+}
+
+pub fn save_projects(projects: Vec<Project>) {
+    let mut projects_dir = dirs::config_dir().unwrap();
+    projects_dir.push(PROJECT_NAME);
+
+    // make sure path exists
+    fs::create_dir_all(projects_dir.clone()).unwrap();
+
+    projects_dir.push(PROJECT_DB_FILENAME);
+
+    // save config to config_dir
+    let contents = serde_json::to_string(&projects).unwrap();
+    fs::write(projects_dir, contents).unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,15 @@
-use std::{fmt, fs};
+use std::fs;
 use std::str::FromStr;
 
-use clap::{ArgEnum, Parser, Subcommand};
-use serde::{Deserialize, Serialize};
+use clap::{Parser, Subcommand};
+use file_handler::{Config, ConfigOptions, FileError, Project};
+
+mod file_handler;
 
 // region -- Project Constants
 const PROJECT_NAME: &str = "fpm";
 const CONFIG_FILENAME: &str = "config.toml";
 const PROJECT_DB_FILENAME: &str = "projects_db.json";
-// endregion
-
-// region -- Custom Errors
-type Result<T> = std::result::Result<T, FileError>;
-
-#[derive(Debug, Clone)]
-enum FileError {
-    LoadingError,
-    ParsingError,
-}
-
-impl fmt::Display for FileError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            FileError::LoadingError => {
-                write!(f, "Failed to load config file")
-            },
-            FileError::ParsingError => {
-                write!(f, "Failed to parse config file")
-            },
-        }
-    }
-}
-// endregion
-
-// region -- Config Struct
-#[derive(Deserialize, Serialize, Default, Debug)]
-struct Config {
-    #[serde(default)]
-    base_dir: Option<String>,
-}
-
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
-enum ConfigOptions {
-    BaseDir,
-}
-// endregion
-
-// region -- Project Struct
-#[derive(Deserialize, Serialize, Default, Debug)]
-struct Project {
-    name:      String,
-    directory: String,
-    #[serde(default)]
-    category:  Option<String>,
-    #[serde(default)]
-    p_type:    Option<String>,
-}
 // endregion
 
 // region -- CLI Structs
@@ -124,7 +78,7 @@ fn main() {
         },
     };
 
-    let mut projects = match load_projects() {
+    let mut projects = match file_handler::load_projects() {
         Ok(p) => p,
         Err(FileError::LoadingError) => {
             vec![]
@@ -144,6 +98,7 @@ fn main() {
             p_type,
             category,
             directory,
+            template,
         } => {
             if config.base_dir.is_none() && directory.is_none() {
                 eprintln!("No directory was specified, and the global Base Directory is not Set.");
@@ -177,7 +132,7 @@ fn main() {
                 category:  category.clone(),
                 p_type:    p_type.clone(),
             });
-            save_projects(projects);
+            file_handler::save_projects(projects);
         },
         Commands::Config { command } => match &command {
             ConfigCommands::Set { setting, value } => {
@@ -187,69 +142,9 @@ fn main() {
                     },
                 }
 
-                save_config(config);
+                file_handler::save_config(config);
             },
-            ConfigCommands::Init => save_config(config),
+            ConfigCommands::Init => file_handler::save_config(config),
         },
     }
-}
-
-fn load_config() -> Result<Config> {
-    let mut config_dir = dirs::config_dir().unwrap();
-    config_dir.push(PROJECT_NAME);
-    config_dir.push(CONFIG_FILENAME);
-
-    let contents = match fs::read_to_string(config_dir) {
-        Ok(c) => Ok(c),
-        Err(_) => Err(FileError::LoadingError),
-    }?;
-
-    match toml::from_str(&contents) {
-        Ok(d) => Ok(d),
-        Err(_) => Err(FileError::ParsingError),
-    }
-}
-
-fn load_projects() -> Result<Vec<Project>> {
-    let mut projects_dir = dirs::config_dir().unwrap();
-    projects_dir.push(PROJECT_NAME);
-    projects_dir.push(PROJECT_DB_FILENAME);
-
-    let contents = match fs::read_to_string(projects_dir) {
-        Ok(c) => Ok(c),
-        Err(_) => Err(FileError::LoadingError),
-    }?;
-
-    match serde_json::from_str(&contents) {
-        Ok(d) => Ok(d),
-        Err(_) => Err(FileError::ParsingError),
-    }
-}
-
-fn save_config(config: Config) {
-    let mut config_dir = dirs::config_dir().unwrap();
-    config_dir.push(PROJECT_NAME);
-
-    // make sure path exists
-    fs::create_dir_all(config_dir.clone()).unwrap();
-
-    config_dir.push(CONFIG_FILENAME);
-
-    // save config to config_dir
-    let contents = toml::to_string(&config).unwrap();
-    fs::write(config_dir, contents).unwrap();
-}
-
-fn save_projects(projects: Vec<Project>) {
-    let mut projects_dir = dirs::config_dir().unwrap();
-    projects_dir.push(PROJECT_NAME);
-
-    // make sure path exists
-    fs::create_dir_all(projects_dir.clone()).unwrap();
-
-    projects_dir.push(PROJECT_DB_FILENAME);
-
-    // save config to config_dir
-    let contents = serde_json::to_string(&projects).unwrap();
-    fs::write(projects_dir, contents).unwrap();
 }
