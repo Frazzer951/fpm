205dbd8037969b21498f42338bf34ab5120bad9a
Luke Eltiste <luke343279@gmail.com>
2023-02-02 15:41:42 -0800

    bug: Put project first in lib.rs so that it compile properly with turbosql

--- fpm-lib/src/lib.rs
+++ fpm-lib/src/lib.rs
@@ -1,8 +1,10 @@
 #![warn(clippy::unwrap_used, clippy::expect_used)]
 
+// Must be included first to not cause compile error
+pub mod project;
+
 pub mod config;
 pub mod database;
 pub mod error;
-pub mod project;
 
 mod utils;
